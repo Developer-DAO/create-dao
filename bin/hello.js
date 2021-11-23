6aa7d6278ff49b8bbef4903932c78b0b0cc7bed7
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2021-11-23 23:29:29 +0530

    Take input from cli

--- bin/hello.js
+++ bin/hello.js
@@ -1,3 +1,16 @@
 #! /usr/bin/env node
 
-console.log('Hello from `create-dao`');
+// console.log('Hello from `create-dao`');
+const inquirer = require('inquirer')
+
+var questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "What's your DAO name?"
+  }
+]
+
+inquirer.prompt(questions).then(answers => {
+  console.log(`${answers['name']} DAO created!`)
+})
