c1c8881a1320d6f788eff32fcfe8315f3c03a38c
henripal <henri.palacci@gmail.com>
2021-11-26 21:43:12 -0500

    simple child process based cli

---
+++ bin/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const child_process = require('child_process')
+
+let args = process.argv
+
+child_process.spawn('./node_modules/.bin/create-next-app',
+    [...(args[2] ? [args[2]] :[]), // skip the first argument if undefined
+    '--example=https://github.com/Developer-DAO/create-dao/tree/main/template'],
+{
+    cwd: process.cwd(),
+    detached: false,
+    stdio: "inherit"
+})
