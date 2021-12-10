88ed4102197a367f085ab8d448d1a5b5ced75b9b
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-12-10 16:43:27 +0530

    Improve mint link

--- template/pages/index.js
+++ template/pages/index.js
@@ -1,16 +1,27 @@
 import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 import Greeter from '../src/artifacts/contracts/Greeter.sol/Greeter.json';
-import { Container, Link as ChakraLink, VStack } from '@chakra-ui/react';
+import {
+  Button,
+  Container,
+  Link as ChakraLink,
+  VStack,
+} from '@chakra-ui/react';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <Container>
       <VStack mt="50">
-        <ChakraLink as={Link} href="/mint">
-          Mint here
-        </ChakraLink>
+        <Button>
+          <ChakraLink
+            as={Link}
+            href="/mint"
+            style={{ textDecoration: 'underline' }}
+          >
+            Mint here
+          </ChakraLink>
+        </Button>
       </VStack>
     </Container>
   );
