44554b6e6ddb8334305884549774b8e6a855990c
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-12-21 16:14:47 +0530

    Clean-up the `deploy` script

--- packages/create-dao/templates/default/scripts/deploy.js
+++ packages/create-dao/templates/default/scripts/deploy.js
@@ -1,37 +1,17 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require('hardhat');
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
-  // We get the contracts to deploy
   const ERC20Token = await hre.ethers.getContractFactory('ERC20Token');
   const token = await ERC20Token.deploy();
   await token.deployed();
   console.log('Token deployed to:', token.address);
 
   const NFT = await hre.ethers.getContractFactory('NFT');
-  const nft = await NFT.deploy('test', 'test');
+  const nft = await NFT.deploy('test', 'test', 10000);
   await nft.deployed();
   console.log('NFT deployed to:', nft.address);
-
-  const Greeter = await hre.ethers.getContractFactory('Greeter');
-  const greeter = await Greeter.deploy('Hello, Hardhat!');
-  await greeter.deployed();
-  console.log('Greeter deployed to:', greeter.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
